2edc849e4af7dcb19f8c123ade2869c0f2be6b89
Nathaniel Fredericks <me@nathfreder.dev>
2019-12-16 17:40:54 -0400

    Implement game join and start functionality

--- game.js
+++ game.js
@@ -1,10 +1,14 @@
 var friendlyWords = require('friendly-words');
 
+var sio;
+
 /**
  * Initialize game
  */
 exports.initGame = function(io, socket) {
+    sio = io;
     socket.on('newGame', newGame);
+    socket.on('joinGame', joinGame)
 }
 
 /**
@@ -20,4 +24,42 @@ function newGame() {
     });
     /* join game room */
     this.join(gameId);
+}
+
+/**
+ * Join existing game
+ */
+function joinGame(data) {
+    var gameId = data.gameId;
+    var rooms = this.adapter.rooms;
+
+    /* check if room exists */
+    if (!rooms[gameId]) {
+        /* room doesn't exist */
+        return;
+    }
+
+    /* check if room is full (max 2 players) */
+    if (Object.keys(rooms[gameId].sockets).length >= 2) {
+        /* room is full */
+        return;
+    }
+
+    /* join game */
+    this.join(gameId);
+
+    /* start game */
+    startGame(gameId);
+}
+
+/**
+ * Start game
+ */
+function startGame(gameId) {
+    /* notify players that game is starting soon */
+    sio.in(gameId).emit('showCountdown')
+    /* count down and start game */
+    setTimeout(function() {
+        sio.in(gameId).emit('startGame')
+    }, 3000);
 }
--- public/javascripts/game.js
+++ public/javascripts/game.js
@@ -7,12 +7,36 @@ function newGame() {
     socket.emit('newGame');
 }
 
+/**
+ * Join existing game
+ */
+function joinGame() {
+    var gameId = prompt('Game ID')
+    socket.emit('joinGame', {
+        gameId,
+    })
+}
+
 socket.on('newGameCreated', function(data) {
     var gameId = data.gameId;
-    /* hide new game button */
-    document.getElementById('newGame').setAttribute('class', 'hidden');
+    /* hide landing */
+    document.getElementById('landing').setAttribute('class', 'hidden');
     /* unhide waiting room */
     document.getElementById('waitingForPlayers').setAttribute('class', '');
     /* show game id */
     document.getElementById('gameId').innerText = gameId;
 });
+
+socket.on('showCountdown', function() {
+    /* hide landing */
+    document.getElementById('landing').setAttribute('class', 'hidden');
+    /* hide waiting room */
+    document.getElementById('waitingForPlayers').setAttribute('class', 'hidden');
+    /* show countdown */
+    document.getElementById('gameCountdown').setAttribute('class', '');
+})
+
+socket.on('startGame', function() {
+    /* hide countdown */
+    document.getElementById('gameCountdown').setAttribute('class', 'hidden');
+})
