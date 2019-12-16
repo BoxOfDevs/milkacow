bc907e7abb3c1c69e98e67162eb9dadb7dbd9396
Nathaniel Fredericks <me@nathfreder.dev>
2019-12-16 16:30:40 -0400

    Add waiting room

--- public/javascripts/game.js
+++ public/javascripts/game.js
@@ -4,5 +4,15 @@ var socket = io();
  * Create new game
  */
 function newGame() {
-    socket.emit('newGame')
+    socket.emit('newGame');
 }
+
+socket.on('newGameCreated', function(data) {
+    var gameId = data.gameId;
+    /* hide new game button */
+    document.getElementById('newGame').setAttribute('class', 'hidden');
+    /* unhide waiting room */
+    document.getElementById('waitingForPlayers').setAttribute('class', '');
+    /* show game id */
+    document.getElementById('gameId').innerText = gameId;
+});
